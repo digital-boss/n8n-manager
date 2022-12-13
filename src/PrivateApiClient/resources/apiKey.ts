aebb556dfb4e2947097bef06139f41141112eed1
maestrow <andrew.shmelev@gmail.com>
2022-12-14 01:41:10 +0300

    fix PrivateApiClient: checkOwner should return undefied when HTTP status = 401 (= AxiosError);

--- src/PrivateApiClient/resources/apiKey.ts
+++ src/PrivateApiClient/resources/apiKey.ts
@@ -5,18 +5,11 @@ const url = '/rest/me/api-key';
 
 export class ApiKey extends ResourceBase {
 
-  private errHandler = (err: any) => {
-    console.log(err.config);
-    console.log(err.response.data);
-    throw new Error(err.response.data.message);
-  }
-
   create () {
     return this.httpClient.request({
       url: url,
       method: 'POST',
     })
-    .catch(this.errHandler)
   }
 
   delete () {
@@ -24,7 +17,6 @@ export class ApiKey extends ResourceBase {
       url: url,
       method: 'DELETE',
     })
-    .catch(this.errHandler)
   }
 
 }
