78b08e5e768c13b0ec07add1387412cd3f0fd620
Stams89 <stanimira.rikova@digital-boss.de>
2024-02-08 00:12:50 +0200

    split bug function into small ones

--- src/WorkflowUpdater.ts
+++ src/WorkflowUpdater.ts
@@ -2,12 +2,12 @@ import fs from 'node:fs';
 import path from 'node:path';
 
 import { converters } from "src/lib/WorkflowUpdater/converters";
-import { ChangesReport, TodoItem } from "./lib/WorkflowUpdater/types";
+
+import type { ChangesReport, TodoItem } from "./lib/WorkflowUpdater/types";
 import { transform } from "./lib/WorkflowUpdater/index";
 import { generateChangesReport } from "./lib/WorkflowUpdater/report";
 
 export function updateWorkflows(dir: string, outputDir: string) {
-
   if (!fs.existsSync(outputDir)) {
     fs.mkdirSync(outputDir);
   }
@@ -20,52 +20,60 @@ export function updateWorkflows(dir: string, outputDir: string) {
   const files = fs.readdirSync(dir);
 
   files.forEach((file: string) => {
-    const filePath = path.join(dir, file);
+    processFile(dir, file, changesReport, outputDir);
+  });
 
-    if (fs.existsSync(filePath)) {
-      try {
-        const workflowData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-        const nodes = workflowData.nodes;
-        const workflowName = file.replace(/\.json$/, '');
+  const changesReportFilePath = path.join(outputDir, 'ChangesReport.md');
+  generateChangesReport(changesReport, changesReportFilePath);
+}
 
-        const modifiedNodes = transform(converters, nodes);
+function processFile(dir: string, file: string, changesReport: ChangesReport, outputDir: string) {
+  const filePath = path.join(dir, file);
 
-        modifiedNodes.forEach((modifiedNode) => {
-          const existingChange = changesReport.changes.find((change) => change.workflowName === workflowName);
-          if (existingChange) {
-            existingChange.nodeNames.push(modifiedNode.nodeName);
-          } else {
-            changesReport.changes.push({ workflowName: workflowName, nodeNames: [modifiedNode.nodeName] });
-          }
+  if (!fs.existsSync(filePath)) {
+    console.error('File does not exist:', filePath);
+    return;
+  }
 
-          if (modifiedNode.additionalText !== "") {
-            const todoItem: TodoItem = {
-              workflow: workflowName,
-              node: modifiedNode.nodeName,
-              additionalText: modifiedNode.additionalText,
-              nodeType: modifiedNode.type,
-            };
-            const existingTodo = changesReport.todos.find((todo) => todo.workflow === workflowName);
-            if (existingTodo) {
-              existingTodo.nodes.push(todoItem);
-            } else {
-              changesReport.todos.push({ workflow: workflowName, nodes: [todoItem] });
-            }
-          }
+  try {
+    const workflowData = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+    const nodes = workflowData.nodes;
+    const workflowName = file.replace(/\.json$/, '');
 
-        });
+    const modifiedNodes = transform(converters, nodes);
+    updateChangesReport(modifiedNodes, workflowName, changesReport);
 
-        const updatedFilePath = path.join(outputDir, file);
-        fs.writeFileSync(updatedFilePath, JSON.stringify(workflowData, null, 2));
+    const updatedFilePath = path.join(outputDir, file);
+    fs.writeFileSync(updatedFilePath, JSON.stringify(workflowData, null, 2));
 
-        console.log(`File updated and saved to updatedNodes folder: ${file}`);
-      } catch (parseError) {
-        console.error(`Error processing file ${file}:`, parseError);
-      }
-      const changesReportFilePath = path.join(outputDir, 'ChangesReport.md');
-      generateChangesReport(changesReport, changesReportFilePath);
+    console.log(`File updated and saved to updatedNodes folder: ${file}`);
+  } catch (parseError) {
+    console.error(`Error processing file ${file}:`, parseError);
+  }
+}
+
+function updateChangesReport(modifiedNodes: any[], workflowName: string, changesReport: ChangesReport) {
+  modifiedNodes.forEach((modifiedNode) => {
+    const existingChange = changesReport.changes.find((change) => change.workflowName === workflowName);
+    if (existingChange) {
+      existingChange.nodeNames.push(modifiedNode.nodeName);
     } else {
-      console.error('File does not exist:', filePath);
+      changesReport.changes.push({ workflowName: workflowName, nodeNames: [modifiedNode.nodeName] });
+    }
+
+    if (modifiedNode.todoMessage !== "") {
+      const todoItem: TodoItem = {
+        workflow: workflowName,
+        nodeName: modifiedNode.nodeName,
+        todoMessage: modifiedNode.todoMessage,
+        nodeType: modifiedNode.type,
+      };
+      const existingTodo = changesReport.todos.find((todo) => todo.workflow === workflowName);
+      if (existingTodo) {
+        existingTodo.nodes.push(todoItem);
+      } else {
+        changesReport.todos.push({ workflow: workflowName, nodes: [todoItem] });
+      }
     }
   });
 }
