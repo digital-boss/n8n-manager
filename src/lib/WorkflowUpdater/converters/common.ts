d8943b0f344f4c23a5e141fb8a97952b7bc37c31
Stams89 <stanimira.rikova@digital-boss.de>
2024-02-08 00:07:30 +0200

    extract common functions

---
+++ src/lib/WorkflowUpdater/converters/common.ts
@@ -0,0 +1,34 @@
+import { OperationMappings } from "../types";
+
+export const operationMappings: OperationMappings = {
+  string: {
+    notEqual: "notEquals",
+    isEmpty: "notExists",
+    isNotEmpty: "exists",
+    default: "equals",
+  },
+  number: {
+    smallerEqual: "lte",
+    equal: "equals",
+    notEqual: "notEquals",
+    larger: "gt",
+    largerEqual: "gte",
+    isEmpty: "notExists",
+    isNotEmpty: "exists",
+    default: "lt",
+  },
+  dateTime: {
+    default: "after",
+  },
+  boolean: {
+    notEqual: "notEquals",
+    default: "equals",
+  },
+};
+
+export function setDefaultValues(value: any, defaultValue: any, type: string, operation: string): any {
+  if (type === "string" && (operation === "exists" || operation === "notExists")) {
+    return value || "";
+  }
+  return value || defaultValue;
+}
