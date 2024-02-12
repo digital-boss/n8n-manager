821b15d14d9990bbcdc84f4f1bab7092ed3e547d
Stams89 <stanimira.rikova@digital-boss.de>
2024-02-12 14:49:59 +0200

    fix formatting

--- src/lib/WorkflowUpdater/converters/functionItemNode.ts
+++ src/lib/WorkflowUpdater/converters/functionItemNode.ts
@@ -3,34 +3,33 @@ import type { IConverter, INode } from "../types";
 const checkNodeType = (t: string) => t === 'n8n-nodes-base.functionItem';
 
 const ver1: IConverter = {
-    predicate: (node: INode) => {
-        return checkNodeType(node.type) && node.typeVersion === 1;
-    },
+  predicate: (node: INode) => {
+    return checkNodeType(node.type) && node.typeVersion === 1;
+  },
 
-    convert: (node: INode) => {
-        node.type = 'n8n-nodes-base.code';
-        node.typeVersion = 2;
-        node.parameters.mode = "runOnceForEachItem";
-        node.parameters.jsCode = node.parameters.functionCode.replace(/\bitem\b/g, 'item.json');
+  convert: (node: INode) => {
+    node.type = 'n8n-nodes-base.code';
+    node.typeVersion = 2;
+    node.parameters.mode = "runOnceForEachItem";
+    node.parameters.jsCode = node.parameters.functionCode.replace(/\bitem\b/g, 'item.json');
 
-        delete node.parameters.functionCode;
+    delete node.parameters.functionCode;
 
-        return 'The node needs to be tested manually. Check the access to the input data and the returned format.';
-
-    }
+    return 'The node needs to be tested manually. Check the access to the input data and the returned format.';
+  }
 }
 
 const ver2: IConverter = {
-    predicate: (node: INode) => {
-        return checkNodeType(node.type) && node.typeVersion === 2;
-    },
+  predicate: (node: INode) => {
+    return checkNodeType(node.type) && node.typeVersion === 2;
+  },
 
-    convert: (node: INode) => {
-        return 'Yon need manually update the node';
-    }
+  convert: (node: INode) => {
+    return 'You need manually update the node';
+  }
 }
 
 export const functionItemNodeConv: IConverter[] = [
-    ver1,
-    ver2
+  ver1,
+  ver2
 ];
